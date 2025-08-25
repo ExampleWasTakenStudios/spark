18a91ca9f41f3f952d9be4670d9a6dced55f084e
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2025-08-25 19:11:35 +0200

    confirgure project for shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
